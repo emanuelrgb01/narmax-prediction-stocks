eb4a6e0c3a2622a0c69bc7e0f4db943d4928a5ad
Rocha <rafael.d.rocha@hotmail.com>
2023-09-22 20:33:57 -0300

    add modelo1 para 2 anos

--- matrizes_2anos.m
+++ matrizes_2anos.m
@@ -1,28 +1,27 @@
-function [P_petro,P_vale,P_embr,T_petro,T_vale,T_embr] = matrizes()
-    % Importando dados
-    
+function [P_petro,P_vale,P_embr,T_petro,T_vale,T_embr] = matriz_2anos()
+    % Importando dados 
     petro = readtable("./data/PETR4.SA.csv","ReadVariableNames",false);
     vale = readtable("./data/VALE3.SA.csv","ReadVariableNames",false);
     embr = readtable("./data/EMBR3.SA.csv","ReadVariableNames",false);
     
     % Filtrando os valores para somente o preço de fechamento
-    petro = table2array(petro(:,5));
-    vale = table2array(vale(:,5));
-    embr = table2array(embr(:,5));
+    % Filtrando também somente para os dois ultimos anos
+    petro = table2array(petro(481:990,5));
+    vale = table2array(vale(481:990,5));
+    embr = table2array(embr(481:990,5));
     
     % Matrizes de entrada e treinamento
-    P_petro = zeros(30,90);
-    P_vale = zeros(30,90);
-    P_embr = zeros(30,90);
+    P_petro = zeros(30,42);
+    P_vale = zeros(30,42);
+    P_embr = zeros(30,42);
     
-    T_petro = zeros(10,90);
-    T_vale = zeros(10,90);
-    T_embr = zeros(10,90);
-    
+    T_petro = zeros(10,42);
+    T_vale = zeros(10,42);
+    T_embr = zeros(10,42);
     
     %% Matrizes de entrada e treinamento para as tres redes neurais
     % Sao usados somente os 900 primeiros precos das acoes
-    for i = 1:90
+    for i = 1:42
         % Matriz de entrada rede PETRO
         % Dez primeiras linhas com precos de petro
         % Linhas 11-20 com precos vale
@@ -55,6 +54,3 @@ function [P_petro,P_vale,P_embr,T_petro,T_vale,T_embr] = matrizes()
         T_embr(1:10,i) = embr(10*i+1:10*(i+1),1);
     end
 end
-
-
-
--- modelo1_2anos.m
+++ modelo1_2anos.m
@@ -1,7 +1,7 @@
 clear
 
 % Matrizes para as três redes neurais
-[P_petro,P_vale,P_embr,T_petro,T_vale,T_embr] = matrizes();
+[P_petro,P_vale,P_embr,T_petro,T_vale,T_embr] = matrizes_2anos();
 
 % Criando as redes neurais com 15 neuronios na camada interna
 net_petro = feedforwardnet(15);
@@ -52,7 +52,7 @@ net_vale = init(net_vale);
 net_embr = init(net_embr);
 
 %% Treinamento das redes
-% Rede Petro
+% Rede petro
 net_petro.trainParam.showWindow = true;
 net_petro.layers{1}.dimensions = 15;
 net_petro.layers{1}.transferFcn = 'tansig';
@@ -60,9 +60,46 @@ net_petro.layers{2}.transferFcn = 'purelin';
 net_petro.performFcn = 'mse';
 net_petro.trainFcn = 'trainlm';
 net_petro.trainParam.epochs = 10000;
-net_petro.trainParam.time = 120;
+net_petro.trainParam.time = 1200;
 net_petro.trainParam.lr = 0.2;
-net_petro.trainParam.min_grad = 10^-8;
+net_petro.trainParam.min_grad = 10^-15;
 net_petro.trainParam.max_fail = 1000;
 
 [net_petro, tr] = train(net_petro,P_petro,T_petro)
+
+%%
+% Rede vale
+net_vale.trainParam.showWindow = true;
+net_vale.layers{1}.dimensions = 15;
+net_vale.layers{1}.transferFcn = 'tansig';
+net_vale.layers{2}.transferFcn = 'purelin';
+net_vale.performFcn = 'mse';
+net_vale.trainFcn = 'trainlm';
+net_vale.trainParam.epochs = 10000;
+net_vale.trainParam.time = 1200;
+net_vale.trainParam.lr = 0.2;
+net_vale.trainParam.min_grad = 10^-15;
+net_vale.trainParam.max_fail = 1000;
+
+[net_vale, tr] = train(net_vale,P_vale,T_vale)
+
+%%
+% Rede embr
+net_vale.trainParam.showWindow = true;
+net_vale.layers{1}.dimensions = 15;
+net_vale.layers{1}.transferFcn = 'tansig';
+net_vale.layers{2}.transferFcn = 'purelin';
+net_vale.performFcn = 'mse';
+net_vale.trainFcn = 'trainlm';
+net_vale.trainParam.epochs = 10000;
+net_vale.trainParam.time = 1200;
+net_vale.trainParam.lr = 0.2;
+net_vale.trainParam.min_grad = 10^-15;
+net_vale.trainParam.max_fail = 1000;
+
+[net_vale, tr] = train(net_vale,P_vale,T_vale)
+
+
+
+
+
